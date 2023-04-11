e99319e14960450b883cd927bed2ae7281e8b126
DirectionlessPad <90464923+DirectionlessPad@users.noreply.github.com>
2023-04-11 17:17:02 +0100

    Implement load_openface_hog

--- load_features.py
+++ load_features.py
@@ -8,14 +8,29 @@ def load_openface_features(features_path: Generator) -> pd.DataFrame:
     Iterates over the directory containing data samples. The features
     from each sample are loaded into a dictionary of dataframes.
     """
+
     samples = {}
     for path in features_path:
         str_path = str(path)
-        start = str_path.index("\\")
-        end = str_path.index(".")
+        start = str_path.rindex("\\")
+        end = str_path.rindex(".")
         sample = str_path[start + 1 : end]
-
+        # Read the csv file and store as dataframe.
         sample_df = pd.read_csv(path)
         sample_df.columns = sample_df.columns.str.replace(" ", "")
+        samples[sample] = sample_df
+    return samples
+
+
+def load_openface_hog(features_path: Generator) -> pd.DataFrame:
+    samples = {}
+    for path in features_path:
+        str_path = str(path)
+        start = str_path.rindex("\\")
+        end = str_path.rindex("H")
+        sample = str_path[start + 1 : end]
+        # Read the csv file and store as dataframe.
+        sample_df = pd.read_csv(path, header=None)
+        breakpoint()
         samples[sample] = sample_df
     return samples
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from load_features import load_openface_features
+from load_features import load_openface_features, load_openface_hog
 from pathlib import Path
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
 FEATURES_PATH = Path("openface_features").rglob("*.csv")
+HOG_FEATURES_PATH = Path("openface_features\hog_features").rglob("*.csv")
 
-samples = load_openface_features(FEATURES_PATH)
+# samples = load_openface_features(FEATURES_PATH)
+sample_hog = load_openface_hog(HOG_FEATURES_PATH)

84b4576b8fcd9682ddac5900611afbdbbbd2f75d
DirectionlessPad <patrickmoore937@gmail.com>
2023-04-12 16:39:15 +0100

    Configured black for VScode + tidying

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from load_features import load_openface_features, load_openface_hog
+"""Main script."""
 from pathlib import Path
 import tensorflow as tf
-
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import (
+    datasets,
+    layers,
+    models,
+)
 import matplotlib.pyplot as plt
+from load_features import load_openface_features, load_openface_hog
 
-FEATURES_PATH = Path("openface_features").rglob("*.csv")
-HOG_FEATURES_PATH = Path("openface_features\hog_features").rglob("*.csv")
 
-# samples = load_openface_features(FEATURES_PATH)
+FEATURES_PATH = Path("openface_features")
+HOG_FEATURES_PATH = Path("openface_features\\hog_features")
+
+samples = load_openface_features(FEATURES_PATH)
 sample_hog = load_openface_hog(HOG_FEATURES_PATH)
+
+print("complete")
